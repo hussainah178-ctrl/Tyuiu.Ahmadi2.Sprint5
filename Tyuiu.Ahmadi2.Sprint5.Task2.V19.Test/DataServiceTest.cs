9fbd256f2e1c91480a442a63dc3cd73eea4ce0c9
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-26 07:35:19 -0800

    Task2

---
+++ Tyuiu.Ahmadi2.Sprint5.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint5.Task2.V19.Lib;
+using System.IO;
+
+namespace Tyuiu.Ahmadi2.Sprint5.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMatrix()
+        {
+            DataService ds = new DataService();
+
+            int[,] inputMatrix = {
+                {9, 2, 5},
+                {8, 8, 2},
+                {7, 4, 8}
+            };
+
+            int[,] expectedMatrix = {
+                {0, 2, 0},
+                {8, 8, 2},
+                {0, 4, 8}
+            };
+
+            int[,] resultMatrix = ds.GetMatrix(inputMatrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, resultMatrix);
+        }
+
+        [TestMethod]
+        public void ValidSaveToFile()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = {
+                {0, 2, 0},
+                {8, 8, 2},
+                {0, 4, 8}
+            };
+
+            string filePath = ds.SaveToFile(matrix);
+
+            // Проверяем что файл создан
+            Assert.IsTrue(File.Exists(filePath));
+
+            // Проверяем содержимое файла
+            string[] fileLines = File.ReadAllLines(filePath);
+            Assert.AreEqual(3, fileLines.Length);
+            Assert.AreEqual("0;2;0", fileLines[0]);
+            Assert.AreEqual("8;8;2", fileLines[1]);
+            Assert.AreEqual("0;4;8", fileLines[2]);
+
+            // Очистка
+            File.Delete(filePath);
+        }
+
+        [TestMethod]
+        public void ValidMatrixToString()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = {
+                {0, 2, 0},
+                {8, 8, 2},
+                {0, 4, 8}
+            };
+
+            string result = ds.MatrixToString(matrix);
+            string expected = " 0; 2; 0\r\n 8; 8; 2\r\n 0; 4; 8";
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.Ahmadi2.Sprint5.Task2.V19/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Tyuiu.Ahmadi2.Sprint5.Task2.V19.Lib
+{
+    public class DataService 
+    {
+        public int[,] GetMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] resultMatrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] % 2 != 0) // если нечетное
+                    {
+                        resultMatrix[i, j] = 0;
+                    }
+                    else
+                    {
+                        resultMatrix[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            return resultMatrix;
+        }
+
+        public string SaveToFile(int[,] matrix)
+        {
+            string tempPath = Path.GetTempPath();
+            string filePath = Path.Combine(tempPath, "OutPutFileTask2.csv");
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                int rows = matrix.GetLength(0);
+                int cols = matrix.GetLength(1);
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        writer.Write(matrix[i, j]);
+                        if (j < cols - 1)
+                            writer.Write(";");
+                    }
+                    writer.WriteLine();
+                }
+            }
+
+            return filePath;
+        }
+
+        public string MatrixToString(int[,] matrix)
+        {
+            string result = "";
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result += matrix[i, j].ToString().PadLeft(2);
+                    if (j < cols - 1)
+                        result += "; ";
+                }
+                if (i < rows - 1)
+                    result += Environment.NewLine;
+            }
+
+            return result;
+        }
+    }
+}
