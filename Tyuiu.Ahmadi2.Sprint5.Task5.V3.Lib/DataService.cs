e2bd4c839d2952d38b05f989fbe73744bd3ba6da
Ahmadi1 <Ahmadi@DS10BK3>
2025-12-08 11:36:04 -0800

    Task5

--- Tyuiu.Ahmadi2.Sprint5.Task5.V3.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint5.Task5.V3.Lib/DataService.cs
@@ -4,7 +4,7 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.Ahmadi2.Sprint5.Task5.V3.Lib
 {
-    public class DataService : ISprint5Task5V3
+    public class DataService :ISprint5Task5V3
     {
         public double LoadFromDataFile(string path)
         {
@@ -15,14 +15,15 @@ namespace Tyuiu.Ahmadi2.Sprint5.Task5.V3.Lib
                 using (StreamReader reader = new StreamReader(path))
                 {
                     string line;
-                    while ((line = reader.ReadLine()) != null)
+                    while ((line = reader.ReadLine())
+                        != null)
                     {
                         line = line.Trim();
                         if (!string.IsNullOrEmpty(line))
                         {
                             line = line.Replace('.', ',');
 
-                            if (double.TryParse(line, out double number))  
+                            if (double.TryParse(line, out double number))
                             {
                                 if (number == (int)number)
                                 {
